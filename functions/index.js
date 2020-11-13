3da77500ec9ef6a5c6e22a6d45690cedfbb554dc
akams <e.ndjawe@gmail.com>
2020-11-13 03:58:58 +0100

    feat: lock api if ur not auth in app

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,9 @@ const admin = require('firebase-admin');
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const cors = require('cors');
 const moment =  require('moment');
+const cookieParser = require('cookie-parser')();
+const cors = require('cors')({origin: true});
 
 admin.initializeApp(functions.config().firebase);
 
@@ -12,14 +13,61 @@ const db = admin.firestore(); // cloudFireStore Db
 const app = express(); // Handle intern API
 const main = express(); // Expose API
 
-main.use(cors());
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const validateFirebaseIdToken = async (req, res, next) => {
+  console.log('Check if request is authorized with Firebase ID token');
+
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+      !(req.cookies && req.cookies.__session)) {
+    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+        'Make sure you authorize your request by providing the following HTTP header:',
+        'Authorization: Bearer <Firebase ID Token>',
+        'or by passing a "__session" cookie.');
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    console.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else if(req.cookies) {
+    console.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  } else {
+    // No cookie
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    console.log('ID Token correctly decoded', decodedIdToken);
+    req.user = decodedIdToken;
+    next();
+    return;
+  } catch (error) {
+    console.error('Error while verifying Firebase ID token:', error);
+    res.status(403).send('Unauthorized');
+    return;
+  }
+};
+
+main.use(cors);
+main.use(cookieParser);
+main.use(validateFirebaseIdToken);
 main.use('/api/v1', app);
 main.use(bodyParser.json());
 
 exports.waatCloudFunction = functions.https.onRequest(main);
 
 app.get('/warmup', (request, response) => {
-  response.send('Warming up serverless .');
+  response.send(`Warming up serverless. Hello ${req.user.name}`);
 });
 
 // signin for waat user
